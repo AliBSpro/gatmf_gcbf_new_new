63a8a6a12c4d026a9c6af98b3b5d67ebbb2f4eff
AliBSpro <156661680+AliBSpro@users.noreply.github.com>
2026-08-30 12:46:17 +0800

    Add files via upload

---
+++ envs/mpe/scenarios/__init__.py
@@ -0,0 +1,14 @@
+import importlib.util
+import os.path as osp
+
+def load(name):
+    pathname = osp.join(osp.dirname(__file__), name)
+    spec = importlib.util.spec_from_file_location("", pathname)
+    if spec is None:
+        raise ImportError(f"Could not load spec from {pathname}")
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    return module
+
+
+
